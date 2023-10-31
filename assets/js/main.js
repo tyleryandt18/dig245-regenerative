067747a468acac2c9854ffe00e69c080ce8f8de7
tyleryandt18 <tyyandt@davidson.edu>
2023-10-30 21:03:22 -0400

    added one minute timer

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,11 @@ TO-DOs:
 - get a list of all possible words, and sort words by popularity, make the computer
 choose the most popular word as well
 - save local state, so if I close a tab, it still comes back to the same state
-- set the message's div to be a constant height so that it doesn't push anything down when it appears
+*/
+
+/*
+Thanks to this github page (too many contributors to list) for the list of words
+https://github.com/dwyl/english-words/tree/master
 */
 
 let wordList = [
@@ -48,6 +52,40 @@ function filterList(currWord) {
     return newWords;
 }
 
+/* 1 minute timer for the player's choice */
+let myTimer = document.getElementById("myTimer");
+let seconds = 60;
+let timerStarted = false;
+let timer;
+
+function playerChoice(){
+    if(timerStarted === false){
+        timerStarted = true;
+        timer = setInterval(function(){
+            if(seconds > 0){
+                if(seconds === 60){
+                    myTimer.textContent = "1:00";
+                } else if(seconds < 10){
+                    myTimer.textContent = "0:0" + String(seconds);
+                } else {
+                    myTimer.textContent = "0:" + String(seconds);
+                }
+                seconds--;
+            } else {
+                clearInterval(timer);
+                timerStarted = false;
+                seconds = 60;
+                myTimer.textContent = "0:00";
+                gameUpdate.textContent = "The timer ran out, you lost the round!";
+                if(updateScore(0)){
+                    gameUpdate.textContent = "GAME OVER! YOU LOST!";
+                    playAgain.style.display = "initial";
+                }
+            }
+        }, 1000)
+    }
+}
+
 let myScore = 0;
 let cpuScore = 0;
 let myCount = document.getElementById("myCount");
@@ -55,6 +93,7 @@ let cpuCount = document.getElementById("cpuCount");
 const GHOST = "GHOST";
 
 function updateScore(flag) {
+    myWord.textContent = '';
     if(flag == 0) {
         myCount.textContent = myCount.textContent + GHOST[myScore];
         myScore++;
@@ -95,6 +134,12 @@ playAgain.addEventListener("click", () => {
 myGame.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    /* clear the timer */
+    timerStarted = false;
+    seconds = 60;
+    myTimer.textContent = "1:00";
+    clearInterval(timer);
+
     this.myAnswer.disabled = true;
     let currWord = myWord.textContent + this.myAnswer.value;
     gameUpdate.textContent = '';
@@ -106,12 +151,12 @@ myGame.addEventListener("submit", (event) => {
             currWord = '';
             if(updateScore(0)){
                 gameUpdate.textContent = "GAME OVER! YOU LOST!";
-                playAgain.style.display = "block";
+                playAgain.style.display = "initial";
             }
         } else {
             let newWords = filterList(currWord);
             if(newWords.length == 0){
-                alert("The word you are spelling is not in the dictionary. Try Again.");
+                gameUpdate.textContent = "The word you are spelling is not in the dictionary. Try again."
                 currWord = myWord.textContent;
             } else {
                 currWord = currWord + computerChoice(newWords, currWord);
@@ -120,16 +165,19 @@ myGame.addEventListener("submit", (event) => {
                         currWord = '';
                         if(updateScore(1)) {
                             currWord = "GAME OVER! YOU WON!";
-                            playAgain.style.display = "block";
+                            playAgain.style.display = "initial";
                         }
+                    } else {
+                        /* 1 minute timer for the player to enter a letter */
+                        playerChoice();
                     }
                 }
     
             }
         myWord.textContent = currWord;
+
     }, 1000);
 
     /* Reset the answer box */
     this.myAnswer.value = '';
-
 });
---
+++ word_list/filter_worddict.py
@@ -0,0 +1,64 @@
+"""
+A Python file that filters the words_dictionary.json file
+in order accord with the rules of GHOST.
+
+Author - Tyler Yandt
+"""
+
+import json
+
+# Test the program before reading over all 500k words in the dictionary
+NUM_LINES_TO_READ = 50
+
+# Open the JSON file
+with open('words_dictionary.json') as file:
+    words_dict = json.load(file)
+    words_list = list(words_dict.keys())
+    filtered_words = []
+
+    # iterate over the words in the .json file, and if that word has at least
+    # 4 characters, add it back to the filtered word list
+    for word in words_list:
+        if len(word) >= 4:
+            filtered_words.append(word)
+
+# Open the frequency.txt file
+# https://ucrel.lancs.ac.uk/bncfreq/flists.html
+word_freq = {}
+first_line = True
+with open('word_freq.txt', 'r', encoding='ISO-8859-1') as file:
+    lines = file.readlines()
+    
+    # Iterate over each word and its frequnecy
+    for line in lines:
+        # strip the line of leading and trailing white space characters
+        words = line.strip().split("\t")
+
+        # This is the header of the file, so we can pass over it
+        if first_line:
+            first_line = False
+            continue
+        
+        # Filtering over some words that we don't need to include in the frequency list
+        if words[1] == 'NoP' or words[1] == 'Num':
+            continue
+
+        word_freq[words[0]] = words[-1]
+
+# the words in the freq list
+freq_words = list(word_freq.keys())
+print(freq_words)
+
+"""
+# write the filtered word list back to the .json file
+with open('words_dictionary.json', 'w') as file:
+    for word in filtered_words:
+
+        # if the word is in the freq_list, then add the frequency to it
+        if word in freq_words:
+            json.dump({word: word_freq[word]}, file, indent = 4)
+        
+        # if not, then just add 1
+        else:
+            json.dump({word: "1"}, file, indent = 4)
+"""
